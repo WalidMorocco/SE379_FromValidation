f46fdc72b0853f864351adc988228047dbae43e3
Walid Amar <walidamarpro@gmail.com>
2022-04-25 02:17:01 -0400

    form with validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import SignUpForm from './Form'
+import {SignUpForm} from './Form'
 
 function App() {
   return (
--- src/Form.js
+++ src/Form.js
@@ -1,61 +1,21 @@
 import './App.css';
+import { Modal } from './Modal';
 import React, { useState } from 'react';
 
 export const SignUpForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
-    const clearForm = (event) => {
-        event.preventDefault();
-        setFirstName('');
-        setLastName('');
-        setEmail('');
+    const onDismissModal = () => {
+        setShowModal(false);
     }
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
-
     return (
-        <div className="form">
-            <form>
-
-                <div className="form-group">
-                    <label className="form-label">First Name: </label>
-                    <input
-                        type="text"
-                        value={firstName}
-                        onChange={(event) => setFirstName(event.target.value)}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label className="form-label">Last Name: </label>
-                    <input
-                        type="text"
-                        value={lastName}
-                        onChange={(event) => setLastName(event.target.value)}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label className="form-label">Email: </label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <button onClick={clearForm} className="button">
-                        Submit
-                    </button>
-                </div>   
-            </form>    
-        </div>
+        <>
+            <div class="card">
+                <button onClick={() => setShowModal(true)} className="btn btn-primary">Form</button>
+                { showModal && <Modal dismissModal={onDismissModal} />}
+            </div>
+            
+        </>
     );
 };
-
-export default SignUpForm;
